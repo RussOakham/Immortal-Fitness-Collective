5d9e4e6ecfe9d311ead040bc506773cd42112539
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-09-05 18:50:02 +0100

    first commit

---
+++ immortal_fitness_collective/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for immortal_fitness_collective project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'immortal_fitness_collective.settings')
+
+application = get_asgi_application()
