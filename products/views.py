89cf98d5c121f018e2d0cff8d363572eb25876d9
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-10-17 16:40:28 +0100

    Search - Added category filter functionality

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.db.models.fields import NullBooleanField
 from django.shortcuts import get_object_or_404, redirect, reverse, render
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,9 +11,15 @@ def all_products(request):
     """ A view to show all products, including sort and search queries """
 
     products = Product.objects.all()
-    query = NullBooleanField
+    query = None
+    category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -27,6 +33,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
