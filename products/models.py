f45ba0ac94cace1797ec46fc46c00bd6d8fe5a49
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-10-23 22:06:16 +0100

    Product - Added Sizes to Model - added size property to model, plus selector to product detail pages and innerHTML templates

--- products/models.py
+++ products/models.py
@@ -1,9 +1,12 @@
+""" required imports for module functionality """
 from django.db import models
 
 
 class Category(models.Model):
+    """ enable categorisation grouping of product catalogue """
 
     class Meta:
+        """ set correct grammar of category plural """
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
@@ -13,14 +16,18 @@ class Category(models.Model):
         return self.name
 
     def get_friendly_name(self):
+        """ get friendly_name of product """
         return self.friendly_name
 
 
 class Product(models.Model):
+    """ set property characteristics of Product class """
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
