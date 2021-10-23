6bf1e5917225d60f614710ec91f39cb2adc79695
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-10-23 22:32:50 +0100

    Product Size Logic - finalised product size logic and updated bag contexts

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,22 +7,37 @@ from products.models import Product
 from immortal_fitness_collective.settings import FREE_DELIVERY_THRESHOLD
 
 def bag_contents(request):
-    """ Context to allow shopping bag contents to be accessed site wide """
+    """
+    Context to allow shopping bag contents to be accessed site wide
+    if item_data is not an integer, it is a dictionary for products with size params
+    """
 
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                'size': size,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
