7e20984e84f9ef3c36466d7ecae6cb79cc80426c
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-10-27 22:00:45 +0100

    Checkout - View and Template - added checkout view model and HTML templates with placeholder styling

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+""" required imports for module functionality """
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
+
 
 # Create your views here.
+def checkout(request):
+    """ A view to return the checkout page """
+
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products)'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,4 @@
+""" required imports for module functionality """
 from django.urls import path
 from . import views
 
