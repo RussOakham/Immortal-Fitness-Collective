8a42f5941e991d3be9843a4f3a65b0ccd5ab1131
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-10-28 11:44:29 +0100

    Env Variables - updated environmental variables and gitignore

--- immortal_fitness_collective/settings.py
+++ immortal_fitness_collective/settings.py
@@ -173,8 +173,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
