b8c9357c24659d2bc9fd742661e1414dbfad3f4f
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-10-30 16:11:10 +0100

    Checkout - Added Stripe Payment Functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -3,8 +3,8 @@
     https://stripe.com/docs/libraries
 */
 
-var stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
-var client_secret = $("#id_client_secret").text().slice(1, -1);
+var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+var clientSecret = $("#id_client_secret").text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 
@@ -25,17 +25,53 @@ var card = elements.create("card", { style: style });
 card.mount("#checkout__card-element");
 
 // Handle realtime validation errors on the card element
-card.addEventListener('change', function(event) {
-    var errorDiv = document.getElementById('card-errors');
-    if (event.error) {
-        var html = `
+card.addEventListener("change", function (event) {
+  var errorDiv = document.getElementById("card-errors");
+  if (event.error) {
+    var html = `
             <span class="icon">
                 <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
         `;
+    $(errorDiv).html(html);
+  } else {
+    errorDiv.textContent = "";
+  }
+});
+
+// Handle form submit
+
+var form = document.getElementById("payment-form");
+
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  card.update({ disabled: true });
+  $("#submit-button").attr("disabled", true);
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+      },
+    })
+    .then(function (result) {
+      if (result.error) {
+        // Show error to customer
+        var errorDiv = document.getElementById("card-errors");
+        var html = `
+            <span class="icon">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${result.error.message}</span>
+        `;
         $(errorDiv).html(html);
-    } else {
-        errorDiv.textContent = '';
-    }
+        card.update({ disable: false });
+        $("#submit-button").attr("disabled", false);
+      } else {
+        // Payment has been processed!
+        if (result.paymentItentn.status === "succeded") {
+          form.submit();
+        }
+      }
+    });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -30,14 +30,12 @@ def checkout(request):
         currency = settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JpUq0DVqRdGh7w3JEexTKIE9JItxaLO7K3tHSPvVykufUNmoxXxXOrcNZGAUyRw5o9qDKwcNkMnMvv8WFNWTYfB00PM99E3Vf',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
