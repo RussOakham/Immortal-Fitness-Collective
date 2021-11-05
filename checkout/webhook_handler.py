7d6b359bfc03694c871a34b6883e8c124563f7d4
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-05 12:22:55 +0000

    Checkout Email Confirmation - Confrimation script setup - added confirmation email text and scrits to webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,9 @@
 import json
 import time
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from products.models import Product
 from profiles.models import UserProfile
@@ -13,6 +16,25 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """ Send use a confirmation email """
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order':order}
+        )
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
+        )
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email],
+        )
 
     def handle_event(self, event):
         """
@@ -80,6 +102,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]}' | 'SUCCESS: \
                     Verified order already in database', status=200)
@@ -124,9 +147,10 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}', status=500)
 
-            return HttpResponse(
-                content=f'Webhook received: {event["type"]} | \
-                    SUCCESS: Created order in webhook', status=200)
+        self._send_confirmation_email(order)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | \
+                SUCCESS: Created order in webhook', status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
--- immortal_fitness_collective/settings.py
+++ immortal_fitness_collective/settings.py
@@ -180,6 +180,7 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'immortalfitnesscollective@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
