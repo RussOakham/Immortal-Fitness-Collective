03b38da9b7ebc73ef3c6a4268a47b4b712ac9bba
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-05 11:46:33 +0000

    Profile - Order History and Checkout - Updated order history and checkout view to save customer info and display historical order information

--- profiles/forms.py
+++ profiles/forms.py
@@ -34,5 +34,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile__form-input'
             self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,21 +1,48 @@
 """ required imports for module functionality """
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 # Create your views here.
 def profile(request):
     """ Display users profile """
-
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated Successfully!')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
         'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ Display users order history """
+
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.info(request, (
+        f'This is a past order confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
