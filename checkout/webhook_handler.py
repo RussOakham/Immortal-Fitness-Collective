d0de322e09ff1fe02c9d772cd4ddb4a53ce561b8
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-07 20:51:52 +0000

    Webhook - Small bug fixes corrected

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -47,14 +47,14 @@ class StripeWH_Handler:
         """
         Handle a payment_intent.succeeded webhook from Stripe
         """
-
+        
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
         billing_details = intent.charges.data[0].billing_details
-        shipping_details = intent.shipping_details
+        shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in shipping details
@@ -68,13 +68,13 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.phone_number__iexact = shipping_details.phone
-                profile.country__iexact = shipping_details.address.country
-                profile.postcode__iexact = shipping_details.address.postal_code
-                profile.town_or_city__iexact = shipping_details.address.city
-                profile.street_address1__iexact = shipping_details.address.line1
-                profile.street_address2__iexact = shipping_details.address.line2
-                profile.county__iexact = shipping_details.address.state
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
                 profile.save()
 
         order_exists = False
@@ -104,8 +104,9 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]}' | 'SUCCESS: \
-                    Verified order already in database', status=200)
+                content=f'Webhook received: {event["type"]} | \
+                    SUCCESS: Verified order already in database',
+                status=200)
         else:
             order = None
             try:
@@ -145,12 +146,12 @@ class StripeWH_Handler:
                 if order:
                     order.delete()
                 return HttpResponse(
-                    content=f'Webhook received: {event["type"]} | ERROR: {e}', status=500)
-
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | \
-                SUCCESS: Created order in webhook', status=200)
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -19,7 +19,7 @@ def webhook(request):
 
     # Get webhook data and verify signature
     payload = request.body
-    sig_header = request.META('HTTP_STRIPE_SIGNATURE')
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
@@ -44,7 +44,7 @@ def webhook(request):
         'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
-    #Get the webhook type from stripe
+    # Get the webhook type from stripe
     event_type = event['type']
 
     # If there's a handler for it, get it from the event map
