e1f9897da59a9565d348dd4814e54010a71764a5
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-13 14:52:32 +0000

    Product Reviews - Removed Edit Review functionality from app

--- products/views.py
+++ products/views.py
@@ -191,33 +191,3 @@ def add_review(request, product_id):
 
     return render(request, context)
 
-
-@login_required
-def edit_review(request, review_id):
-    """ Edit a existing review """
-    review = get_object_or_404(ProductReview, pk=review_id)
-    product = review.product
-
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            form.save()
-            messages.info(request, 'Review updated!')
-            return redirect(reverse('product_detail', args=[product.id]))
-        else:
-            messages.error(
-                request, "Failed to update review, please ensure form is valid")
-
-    else:
-        form = ReviewForm(instance=review)
-
-    messages.info(request, f'You are editing your review of {product}.')
-    template = 'products/product_detail.html'
-    context = {
-        'form': form,
-        'review': review,
-        'product': product,
-        'edit': True,
-    }
-
-    return render(request, template, context)
