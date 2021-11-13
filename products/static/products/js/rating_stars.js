4e3e2697007a654a060d4256bc55f6ffa462810a
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-13 16:31:14 +0000

    Product Reviews - Styling - styled review stars and hover interaction

--- products/static/products/js/rating_stars.js
+++ products/static/products/js/rating_stars.js
@@ -2,11 +2,12 @@ const productStars = document.querySelectorAll(".review-card__title-stars");
 
 productStars.forEach(rating => {
     const ratingValue = parseInt(rating.dataset.value);
+    const ratingStar = ratingValue;
 
     let stars = '';
 
     for (let i = 1; i < 6; i++) {
-        if (i <= ratingValue) {
+        if (i <= ratingStar) {
             stars += `<i class="fas fa-star"></i>`;
         } else {
             stars += `<i class="far fa-star"></i>`;
