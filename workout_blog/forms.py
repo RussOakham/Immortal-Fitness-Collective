0b75460c4cea79f4fc919de53a46cbb33f15633d
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-14 17:47:14 +0000

    Add Workout - View, Form and Templates - added View, Form and Template for 'add_workout' functionality

---
+++ workout_blog/forms.py
@@ -0,0 +1,23 @@
+""" required imports for module functionality """
+from django import forms
+from .models import Workout, WorkoutProgramme
+
+
+class WorkoutForm(forms.ModelForm):
+    """ Create Workout so admins can update, add and delete Workouts """
+
+    class Meta:
+        """ Retrieve fields for form """
+        model = Workout
+        fields = '__all__'
+        exclude = ('author', 'upload_date',)
+
+    def __init__(self, *args, **kwargs):
+        """ Override init to retrieve fields by friendly_name """
+        super().__init__(*args, **kwargs)
+        categories = WorkoutProgramme.objects.all()
+        friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_name
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- workout_blog/views.py
+++ workout_blog/views.py
@@ -1,10 +1,9 @@
 """ required imports for module functionality """
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from immortal_fitness_collective.settings import TEMPLATES
-
+from .forms import WorkoutForm
 from .models import Workout, WorkoutProgramme
 
 # Create your views here.
@@ -39,3 +38,61 @@ def workout_detail(request, slug):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def add_workout(request):
+    """ Add a new workout post """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only admins can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST)
+        if form.is_valid():
+            workout = form.save()
+            messages.success(request, 'Successfully added new workout.')
+            return redirect(reverse('workout_detail', args=[workout.slug]))
+        else:
+            messages.error(request, 'Failed to add new Workout. Please ensure form is valid.')
+
+    else:
+        form = WorkoutForm()
+
+    template = 'workout_blog/add_workout.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+# @login_required
+# def edit_workout(request, slug):
+#     """ Edit a workout """
+
+#     if not request.user.is_superuser:
+#         messages.error(request, 'Sorry, only admins can do that!')
+
+#     workout = get_object_or_404(Workout, slug=slug)
+#     if request.method == 'POST':
+#         form = WorkoutForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Successfully updated workout.')
+#             return redirect(reverse('workout_detail', args=[workout.slug]))
+#         else:
+#             messages.error(request, 'Failed to update workout. Please ensure form is valid.')
+
+#     else:
+#         form = WorkoutForm(instance=workout_detail)
+#         messages.info(request, f'You are editing {workout.title}')
+
+#     template = 'workout_blog/edit_workout.html'
+#     context = {
+#         'form': form,
+#         'workout': workout,
+#     }
+
+#     return render(request, template, context)
