a0b1ed44f64d232ea16ec19abbd78ce047598448
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-14 15:54:14 +0000

    Workout Programmes - Page H1 - updated view and template to populate H1 with programme name

--- workout_blog/views.py
+++ workout_blog/views.py
@@ -1,16 +1,23 @@
 """ required imports for module functionality """
 from django.shortcuts import render
-from .models import Workout
+from .models import Workout, WorkoutProgramme
 
 # Create your views here.
 def all_workouts(request):
     """ A view to show all workout blogs uploaded """
 
     workouts = Workout.objects.all()
+    categories = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        workouts = workouts.filter(category__name__in=categories)
+        categories = WorkoutProgramme.objects.filter(name__in=categories)
 
     template = 'workout_blog/workouts.html'
     context = {
         'workouts': workouts,
+        'current_category': categories,
     }
 
     return render(request, template, context)
