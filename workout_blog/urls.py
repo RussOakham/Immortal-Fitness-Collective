d748b183f423f1f3310138670cfe3cbbaef9d141
RussOakham <73036915+RussOakham@users.noreply.github.com>
2021-11-14 16:10:15 +0000

    Workout Detail - View, URL and Template - Added Workout_Detail view, url and template

--- workout_blog/urls.py
+++ workout_blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_workouts, name='all_workouts'),
+    path('workout_detail/<slug:slug>', views.workout_detail, name='workout_detail'),
 ]
--- workout_blog/views.py
+++ workout_blog/views.py
@@ -1,5 +1,10 @@
 """ required imports for module functionality """
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from immortal_fitness_collective.settings import TEMPLATES
+
 from .models import Workout, WorkoutProgramme
 
 # Create your views here.
@@ -18,6 +23,19 @@ def all_workouts(request):
     context = {
         'workouts': workouts,
         'current_category': categories,
+    }
+
+    return render(request, template, context)
+
+
+def workout_detail(request, slug):
+    """ A view to show workout detail """
+
+    workout = get_object_or_404(Workout, slug=slug)
+
+    template = 'workout_blog/workout_detail.html'
+    context = {
+        'workout': workout,
     }
 
     return render(request, template, context)
